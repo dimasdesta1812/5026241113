635f7bf40508e40ceabf7d8638ea3ca65c2ed7b0
Dimas Ananda Destariansyah <dimasdesta1812@gmail.com>
2026-04-27 00:12:39 +0700

    tugas pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,32 @@ Route::get('blog', function () {
 	return view('blog');
 });
 
+Route::get('pertemuan1', function () {
+	return view('pertemuan1');
+});
+
+Route::get('pertemuan2', function () {
+	return view('pertemuan2');
+});
+
+Route::get('pertemuan3', function () {
+	return view('pertemuan3');
+});
+
+Route::get('pertemuan4', function () {
+	return view('pertemuan4');
+});
+
 Route::get('pert5', function () {
 	return view('pertemuan5');
+});
+
+Route::get('tugaslinktree', function () {
+	return view('tugaslinktree');
+});
+
+Route::get('tugasbootstrap', function () {
+	return view('tugasbootstrap');
 });
 
 Route::get('dosen', [DosenController::class, 'index']);
