3e9358bc8204121d0a8afa981c5c30400ea133de
Dimas Ananda Destariansyah <dimasdesta1812@gmail.com>
2026-05-25 12:04:29 +0700

    pertemuan 12

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -20,7 +20,7 @@
         <nav class="navbar navbar-expand-sm bg-light navbar-light">
             <div class="container-fluid">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
+                    <li class="nav-item"> 
                         <a class="nav-link active" href="/pegawai">Pegawai</a>
                     </li>
                     <li class="nav-item">
