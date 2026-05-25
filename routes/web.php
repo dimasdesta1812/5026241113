f9e0c2868384e20012056ccf2fd3f3a6ca59a8ae
Dimas Ananda Destariansyah <dimasdesta1812@gmail.com>
2026-05-25 12:06:14 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -70,4 +70,4 @@ Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
 Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
 Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
 Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
-Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']); 
